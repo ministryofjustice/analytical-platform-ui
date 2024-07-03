9e3db7ad979e8e53d83db6f4ef397df77865dc97
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-07-03 10:07:16 +0000

    Move NGINX config to chart
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- container/src/usr/local/bin/init-nginx.sh
+++ container/src/usr/local/bin/init-nginx.sh
@@ -3,7 +3,7 @@
 # This is a helper script used in Analytical Platform UI's Kubernetes deployment
 # It copies the static assets into a shared volume for the NGINX container to serve
 
-export STATIC_SRC="${STATIC_SRC:-"/ap/static"}"
+export STATIC_SRC="${STATIC_SRC:-"/app/run/static"}"
 export STATIC_DEST="${STATIC_DEST:-"/staticfiles"}"
 
 cp -R "${STATIC_SRC}" "${STATIC_DEST}"
