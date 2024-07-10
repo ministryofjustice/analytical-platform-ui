4a519ae13a824dc2b398812734db9e4a94fe8e24
Michael Collins <15347726+michaeljcollinsuk@users.noreply.github.com>
2024-07-10 11:54:23 +0000

    Fix minor bugs
    - Fix bug in context_processors when user not authenticated
    - Add healthcheck url

--- ap/views.py
+++ ap/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponse
+from django.views import View
 from django.views.generic import TemplateView
 
 from ap.auth.views.mixins import OIDCLoginRequiredMixin
@@ -5,3 +7,8 @@ from ap.auth.views.mixins import OIDCLoginRequiredMixin
 
 class IndexView(OIDCLoginRequiredMixin, TemplateView):
     template_name = "home.html"
+
+
+class HealthcheckView(View):
+    def get(self, request):
+        return HttpResponse("OK")
