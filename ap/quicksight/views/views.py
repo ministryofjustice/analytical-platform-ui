6e3fc69a24f8aa6f506bf36268ff073c4d16f328
Michael Collins <15347726+michaeljcollinsuk@users.noreply.github.com>
2024-08-05 15:33:04 +0000

    Refactor quicksight embed code
    Remove call to get users quicksight ARN

--- ap/quicksight/views/views.py
+++ ap/quicksight/views/views.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.views.generic import TemplateView
 
 import boto3
+import botocore
+import sentry_sdk
 
 from ap.auth.views.mixins import OIDCLoginRequiredMixin
 
@@ -11,49 +13,40 @@ class QuicksightView(OIDCLoginRequiredMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        user = self.request.user
-        context["embed_url"] = self._get_embed_url(user)
+        context["embed_url"] = self._get_embed_url(user=self.request.user)
         return context
 
     def _get_embed_url(self, user):
-        qs_session_name = f"quicksight-test-{user.email}"
-
         sts = boto3.client("sts")
+        # TODO update as neccessary. This is a temporary solution as our local roles dont have
+        # quicksight permissions. If we update the app to use a role both locally and in dev/prod
+        # this assume_role call will not be necessary
         response = sts.assume_role(
             RoleArn=f"arn:aws:iam::{settings.COMPUTE_ACCOUNT_ID}:role/quicksight_test",
-            RoleSessionName=qs_session_name,
+            RoleSessionName=f"quicksight-session-{user.email}",
         )
-
         qs_credentials = {
             "aws_access_key_id": response["Credentials"]["AccessKeyId"],
             "aws_secret_access_key": response["Credentials"]["SecretAccessKey"],
             "aws_session_token": response["Credentials"]["SessionToken"],
         }
 
-        quicksight = boto3.client("quicksight", **qs_credentials, region_name="eu-west-2")
-
-        # Gets information about members of the aws_analytical_platform group
-        # group_response = quicksight.list_group_memberships(GroupName="aws_analytical_platform",
-        #                                     AwsAccountId=settings.COMPUTE_ACCOUNT_ID,
-        #                                     Namespace="default")
-        # print(group_response)
-
-        # Assumes email is part of the above group
-        user_response = quicksight.describe_user(
-            UserName=user.email, AwsAccountId=settings.COMPUTE_ACCOUNT_ID, Namespace="default"
-        )
-        print(user_response)
-
-        url_response = quicksight.generate_embed_url_for_registered_user(
-            AwsAccountId=settings.COMPUTE_ACCOUNT_ID,
-            UserArn=user_response["User"]["Arn"],
-            ExperienceConfiguration={
-                "QuickSightConsole": {
-                    "InitialPath": "/start",
-                    "FeatureConfigurations": {"StatePersistence": {"Enabled": True}},
+        try:
+            quicksight = boto3.client("quicksight", **qs_credentials, region_name="eu-west-2")
+            url_response = quicksight.generate_embed_url_for_registered_user(
+                AwsAccountId=settings.COMPUTE_ACCOUNT_ID,
+                UserArn=self.request.user.quicksight_arn,
+                ExperienceConfiguration={
+                    "QuickSightConsole": {
+                        "InitialPath": "/start",
+                        "FeatureConfigurations": {"StatePersistence": {"Enabled": True}},
+                    },
                 },
-            },
-            AllowedDomains=["http://localhost:8000"],
-        )
+                AllowedDomains=settings.QUICKSIGHT_DOMAINS,
+            )
+            url = url_response["EmbedUrl"]
+        except botocore.exceptions.ClientError as e:
+            sentry_sdk.capture_exception(e)
+            url = None
 
-        return url_response["EmbedUrl"]
+        return url
