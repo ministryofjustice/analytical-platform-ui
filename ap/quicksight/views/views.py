ffb09c2edbeb587dcdd9f57868e8e9c247cfda00
Michael Collins <15347726+michaeljcollinsuk@users.noreply.github.com>
2024-08-09 11:33:10 +0000

    Add code to ensure single boto3 session created

--- ap/quicksight/views/views.py
+++ ap/quicksight/views/views.py
@@ -2,8 +2,8 @@ from typing import Any
 
 from django.views.generic import TemplateView
 
+from ap import aws
 from ap.auth.views.mixins import OIDCLoginRequiredMixin
-from ap.aws.quicksight import QuicksightAWSService
 
 
 class QuicksightView(OIDCLoginRequiredMixin, TemplateView):
@@ -11,5 +11,5 @@ class QuicksightView(OIDCLoginRequiredMixin, TemplateView):
 
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
-        context["embed_url"] = QuicksightAWSService().get_embed_url(user=self.request.user)
+        context["embed_url"] = aws.QuicksightService().get_embed_url(user=self.request.user)
         return context
