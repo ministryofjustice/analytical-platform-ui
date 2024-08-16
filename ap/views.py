6c611b845ae16eda46b723c93e456722bfa6b25f
Michael Collins <15347726+michaeljcollinsuk@users.noreply.github.com>
2024-08-16 15:45:25 +0000

    Fix redirect when refreshing session
    When session expires redirect the user to the page
    that they were trying to get to.

--- ap/views.py
+++ ap/views.py
@@ -1,10 +1,22 @@
 from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.urls import reverse
 from django.views import View
 from django.views.generic import TemplateView
+
+from ap.auth.oidc import OIDCSessionValidator
 
 
 class IndexView(TemplateView):
     template_name = "home.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        """
+        If the session has expired, redirect to login
+        """
+        if OIDCSessionValidator(request).expired():
+            return redirect(reverse("login"))
+        return super().dispatch(request, *args, **kwargs)
 
     def get_template_names(self) -> list[str]:
         if not self.request.user.is_authenticated:
