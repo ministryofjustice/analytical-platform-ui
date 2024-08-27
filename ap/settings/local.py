3d0d4d7553b224f2a6c3a09c9e20c9f6a16d591f
Michael Collins <15347726+michaeljcollinsuk@users.noreply.github.com>
2024-08-27 14:50:42 +0000

    Add django-debug-toolbar for local running

--- ap/settings/local.py
+++ ap/settings/local.py
@@ -7,3 +7,15 @@ ALLOWED_HOSTS = [".localhost", "127.0.0.1"]
 QUICKSIGHT_DOMAINS = ["http://localhost:8000"]
 
 ENV = "local"
+
+INSTALLED_APPS += [  # noqa
+    "debug_toolbar",
+]
+
+MIDDLEWARE += [  # noqa
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+]
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
