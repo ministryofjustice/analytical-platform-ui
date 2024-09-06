66b0344c7ee1c166a002c0de26b6fc11cb9db21f
Michael Collins <15347726+michaeljcollinsuk@users.noreply.github.com>
2024-09-06 10:46:32 +0000

    Only catch AttributeError in templatetags

--- ap/database_access/templatetags/database_access_tags.py
+++ ap/database_access/templatetags/database_access_tags.py
@@ -8,7 +8,7 @@ register = template.Library()
 def database_detail_url(database):
     try:
         return database.get_absolute_url()
-    except (TypeError, AttributeError):
+    except AttributeError:
         return reverse("database_access:detail", kwargs={"database_name": database["Name"]})
 
 
@@ -16,7 +16,7 @@ def database_detail_url(database):
 def table_detail_url(table, database_name):
     try:
         return table.get_absolute_table_detail_url()
-    except (TypeError, AttributeError):
+    except AttributeError:
         return reverse(
             "database_access:table_detail",
             kwargs={"database_name": database_name, "table_name": table["Name"]},
