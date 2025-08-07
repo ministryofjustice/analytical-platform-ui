1024ebea6c0494f53ac7410f3766805515e00462
Michael Collins <15347726+michaeljcollinsuk@users.noreply.github.com>
2025-08-07 11:47:02 +0000

    Update dev container files

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -euxo pipefail
 
 # Upgrade NPM
 npm install --global npm@latest
@@ -6,22 +7,18 @@ npm install --global npm@latest
 # Start Postgres
 docker compose --file contrib/docker-compose-postgres.yml up --detach
 
-# Upgrade Pip
-pip install --break-system-package --upgrade pip
+# Remove existing virtual environment
+rm -rf .venv
 
-# Install dependencies
-pip install --break-system-package --requirement requirements.dev.txt
+# Install venv, dependencies and run migrations
+uv sync
+uv run python manage.py migrate
 
-# install npm dependencies and static assets
+# # install npm dependencies and static assets
 npm install
 make build-static
 
-# Run migrations
-python manage.py migrate
-
-# create aws and kube configs
+# # create aws and kube configs
 aws-sso login
-aws-sso setup profiles --force
-
 aws-sso exec --profile analytical-platform-compute-development:modernisation-platform-sandbox -- aws eks --region eu-west-2 update-kubeconfig --name analytical-platform-compute-development --alias apc-dev-cluster
 kubectl config use-context apc-dev-cluster
--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -euxo pipefail
 
 # Run pre-commit
-pre-commit install
+uv run pre-commit install
